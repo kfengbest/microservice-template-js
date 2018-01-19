43c2a80e983f1e374b706375f06ada2150964368
fengka <kaven.feng@autodesk.com>
2018-01-19 08:54:56 +0800

    print out

--- jenkins/init.groovy.d/3_add_credential_username_psw_dockerhub.groovy
+++ jenkins/init.groovy.d/3_add_credential_username_psw_dockerhub.groovy
@@ -3,11 +3,12 @@ import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
 
 final String id = 'dockerHub'
-final String description = 'hub.docker.com'
+final String description = ''
 final String username = System.getProperty('DOCKERHUB_USER_NAME')
 final String password = System.getProperty('DOCKERHUB_USER_PASSWORD')
 
 println username
+println password
 
 final Credentials credentials = (Credentials) new UsernamePasswordCredentialsImpl(
     CredentialsScope.GLOBAL, id, description, username, password
