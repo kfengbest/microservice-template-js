98a8b67b9669f0284cfdba5a18b6fc8ef8ca8793
fengka <kaven.feng@autodesk.com>
2018-01-19 11:36:29 +0800

    v4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello microservice template! V3'))
+app.get('/', (req, res) => res.send('Hello microservice template! V4'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
